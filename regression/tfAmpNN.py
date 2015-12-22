4e72a74495d0a3abc9f35237efa3f77580083e8c
Zachary Ulissi <zulissi@gmail.com>
2015-12-22 17:44:35 -0500

    implementation of get_energy function
    based get_energy on the methods in train(), so abstracted shared
    components to generateTensorFlowArrays.
    
    In doing this, I realized that the current system implicitly assumed
    that there was at least one atom of each type in each batch while
    training, which wasn’t great.
    
    I think this is fixed now, by adding a final mask to the results.  if
    there isn’t an atom of a given type present in the current image (or
    batch of images), it passes a fake fingerprint in, and then sets the
    energy to zero of that atom type’s output.
    
    Effectively, unused atom types can be masked.

--- regression/tfAmpNN.py
+++ regression/tfAmpNN.py
@@ -20,12 +20,14 @@ class tfAmpNN:
         #first, define all of the input placeholders to the tf system
         tensordict={}
         indsdict={}
+        maskdict={}
         for element in self.elements:
             tensordict[element]=tf.placeholder("float", shape=[None, self.elementFingerprintLengths[element]])
             indsdict[element]=tf.placeholder("int64", shape=[None])
+            maskdict[element]=tf.placeholder("float",shape=[None])
         self.indsdict=indsdict
         self.tensordict=tensordict
-
+        self.maskdict=maskdict
         #y_ is the known energy of each configuration
         self.y_ = tf.placeholder("float", shape=[None, 1])
         self.keep_prob = tf.placeholder("float")
@@ -40,7 +42,7 @@ class tfAmpNN:
                 networkListToUse=self.hiddenlayers[element]
             else:
                 networkListToUse=self.hiddenlayers
-            outdict[element]=model(tensordict[element],indsdict[element],self.keep_prob,self.batchsizeInput,networkListToUse,self.activation,self.elementFingerprintLengths[element])
+            outdict[element]=model(tensordict[element],indsdict[element],self.keep_prob,self.batchsizeInput,networkListToUse,self.activation,self.elementFingerprintLengths[element],mask=maskdict[element])
         self.outdict=outdict
 
         #The total energy is the sum of the energies over each atom type
@@ -61,60 +63,18 @@ class tfAmpNN:
         self.sess.run(tf.initialize_all_variables())
 
     def train(self,images,fingerprintDB,batchsize=20,nEpochs=100):
-        nAtomsDict={}
-        for element in self.elements:
-            nAtomsDict[element]=np.zeros(len(images))
 
-        keylist=images.keys()
-        for j in range(len(images)):
-            atoms=images[keylist[j]]
-            fp=fingerprintDB[keylist[j]]
-            for i in range(len(fp)):
-                atom=atoms[i]
-                nAtomsDict[atom.symbol][j]+=1
-
-        atomsPositions={}
-        for element in self.elements:
-            atomsPositions[element]=np.cumsum(nAtomsDict[element])-nAtomsDict[element]
-
-        atomsIndsReverse={}
-        for element in self.elements:
-            atomsIndsReverse[element]=[]
-            for i in range(len(keylist)):
-                if nAtomsDict[element][i]>0:
-                    atomsIndsReverse[element].append(np.ones((nAtomsDict[element][i],1))*i)
-            atomsIndsReverse[element]=np.concatenate(atomsIndsReverse[element])
-
-        atomArraysAll={}
-        for element in self.elements:
-            atomArraysAll[element]=[]
-
-        energies=np.zeros((len(images),1))
-        natoms=np.zeros((len(images),1))
-        for j in range(len(images)):
-            atoms=images[keylist[j]]
-            fp=fingerprintDB[keylist[j]]
-            for element in self.elements:
-                atomArraysTemp=[]
-                curatoms=[atom for atom in atoms if atom.symbol==element]
-                for i in range(len(curatoms)):
-                    atomArraysTemp.append(fp[curatoms[i].index])
-                if len(atomArraysTemp)>0:
-                    atomArraysAll[element].append(atomArraysTemp)
-            energies[j]=atoms.get_potential_energy()
-            natoms[j]=len(atoms)
-
-        for element in self.elements:
-            atomArraysAll[element]=np.concatenate(atomArraysAll[element])
-
-        energies=energies
-        energies=energies-np.mean(energies)
-        self.energyScale=np.mean(np.abs(energies))
-
+        atomArraysAll,nAtomsDict,atomsIndsReverse,energies,natoms=generateTensorFlowArrays(images,fingerprintDB,self.elements,images.keys())
+        
+        #energies=energies-np.mean(energies)
+        #energies=energies
+        self.energyMeanScale=np.mean(energies)
+        energies=energies-self.energyMeanScale
+        self.energyProdScale=np.mean(np.abs(energies))
+        energies=energies/self.energyProdScale
         self.elementFPScales={}
         for element in self.elements:
             self.elementFPScales[element]=np.max(np.max(atomArraysAll[element]))
-
 
         def trainmodel(nepoch,trainingrate,keepprob):
             icount=1
@@ -129,9 +89,15 @@ class tfAmpNN:
                     atomArraysFinal,atomInds=generateBatch(curinds,self.elements,atomArraysAll,nAtomsDict,atomsIndsReverse)
                     feedinput={}
                     for element in self.elements:
-                        feedinput[self.tensordict[element]]=atomArraysFinal[element]/self.elementFPScales[element]
-                        feedinput[self.indsdict[element]]=atomInds[element]
-                    feedinput[self.y_]=energies[curinds]/self.energyScale
+                        if len(atomArraysFinal[element])>0:
+                            feedinput[self.tensordict[element]]=atomArraysFinal[element]/self.elementFPScales[element]
+                            feedinput[self.indsdict[element]]=atomInds[element]
+                            feedinput[self.maskdict[element]]=np.ones(batchsize)
+                        else:
+                            feedinput[self.tensordict[element]]=np.zeros((1,self.elementFingerprintLengths[element]))
+                            feedinput[self.indsdict[element]]=[0]
+                            feedinput[self.maskdict[element]]=np.zeros(batchsize)
+                    feedinput[self.y_]=energies[curinds]
                     feedinput[self.batchsizeInput]=batchsize
                     feedinput[self.learningrate]=trainingrate
                     feedinput[self.keep_prob]=keepprob
@@ -142,19 +108,44 @@ class tfAmpNN:
                     
                     #Print the loss function every 100 evals.  Would be better to handle this by evaluating the loss function on the entire dataset, but batchsize is currently hardcoded at the moment
                     if icount%100==0:
-                        print('Loss per atom=%1.3f'%(self.lossPerAtom.eval(feed_dict=feedinput)*self.energyScale))
+                        print('Loss per atom=%1.3f'%(self.lossPerAtom.eval(feed_dict=feedinput)*self.energyProdScale))
                     icount+=1
 
         trainmodel(nEpochs,1.e-4,0.5)
 
     #implement methods to get the energy and forces for a set of configurations
-    #def get_energy():
+    def get_energy(self,hashs,images,fingerprintDB):
+        
+        #make images a list in case we've been passed a single hash to calculate
+        if not(isinstance(hashs,list)):
+            hashs=[hashs]
+        
+        #reformat the image and fingerprint data into something we can pass into tensorflow
+        atomArraysAll,nAtomsDict,atomsIndsReverse,energies,natoms=generateTensorFlowArrays(images,fingerprintDB,self.elements,hashs)
+        
+        energies=np.zeros(len(images))
+        curinds=range(len(hashs))
+        atomArraysFinal,atomInds=generateBatch(curinds,self.elements,atomArraysAll,nAtomsDict,atomsIndsReverse)
+        feedinput={}
+        for element in self.elements:
+            if len(atomArraysFinal[element])>0:
+                feedinput[self.tensordict[element]]=atomArraysFinal[element]/self.elementFPScales[element]
+                feedinput[self.indsdict[element]]=atomInds[element]
+                feedinput[self.maskdict[element]]=np.ones(len(hashs))
+            else:
+                feedinput[self.tensordict[element]]=np.zeros((1,self.elementFingerprintLengths[element]))
+                feedinput[self.indsdict[element]]=[0]
+                feedinput[self.maskdict[element]]=np.zeros(len(hashs))
+        feedinput[self.batchsizeInput]=len(hashs)
+        feedinput[self.nAtoms_in]=natoms[curinds]
+        energies=np.array(self.energy.eval(feed_dict=feedinput))*self.energyProdScale+self.energyMeanScale
+        return energies
 
     #def get_forces():
 
 
 #This function generates a multilayer neural network with variable number of neurons
-def model(x,segmentinds,keep_prob,batchsize,neuronList,activationType,fplength):
+def model(x,segmentinds,keep_prob,batchsize,neuronList,activationType,fplength,mask):
     
     nNeurons=neuronList[0]
     #Pass  the input tensors through the first soft-plus layer
@@ -175,7 +166,8 @@ def model(x,segmentinds,keep_prob,batchsize,neuronList,activationType,fplength):
     y_out=tf.matmul(h_fc, W_fc_out) + b_fc_out
     
     #Sum the predicted energy for each atom
-    return tf.unsorted_segment_sum(y_out,segmentinds,batchsize)
+    reducedSum=tf.unsorted_segment_sum(y_out,segmentinds,batchsize)
+    return tf.mul(reducedSum,mask)
 
 
 #Helper functions taken from the MNIST tutorial to generate weight and bias variables
@@ -187,28 +179,86 @@ def bias_variable(shape):
     initial = tf.constant(0.5, shape=shape)
     return tf.Variable(initial)
 
-#This method generates batches from a large dataset using a set of selected indices curinds.  Pretty slow due to the concatenate call, should be done in tensorflow instead
+#This method generates batches from a large dataset using a set of selected indices curinds.
 def generateBatch(curinds,elements,atomArraysAll,nAtomsDict,atomsIndsReverse):
     atomArrays={}
     for element in elements:
         atomArrays[element]=[]
     atomArraysFinal={}
     
-    
     curNAtoms={}
     for element in elements:
         validKeys=np.in1d(atomsIndsReverse[element],curinds)
         curNAtoms[element]=[]
         for ind in curinds:
-            curNAtoms[element].append(len(atomArraysAll[element][ind]))
-        atomArraysFinal[element]=atomArraysAll[element][validKeys]
-
+            if len(atomArraysAll[element])>0:
+                curNAtoms[element].append(len(atomArraysAll[element][ind]))
+            else:
+                curNAtoms[element].append(0)
+        if len(validKeys)>0:
+            atomArraysFinal[element]=atomArraysAll[element][validKeys]
+        else:
+            atomArraysFinal[element]=[]
     atomInds={}
 
     for element in elements:
         validKeys=np.in1d(atomsIndsReverse[element],curinds)
-        atomIndsTemp=np.sum(atomsIndsReverse[element][validKeys],1)
-        atomInds[element]=atomIndsTemp*0.
-        for i in range(len(curinds)):
-            atomInds[element][atomIndsTemp==curinds[i]]=i
+        if len(validKeys)>0:
+            atomIndsTemp=np.sum(atomsIndsReverse[element][validKeys],1)
+            atomInds[element]=atomIndsTemp*0.
+            for i in range(len(curinds)):
+                atomInds[element][atomIndsTemp==curinds[i]]=i
+        else:
+            atomInds[element]=[]
     return atomArraysFinal,atomInds
+
+def generateTensorFlowArrays(images,fingerprintDB,elements,keylist):
+    nAtomsDict={}
+    for element in elements:
+        nAtomsDict[element]=np.zeros(len(images))
+        
+    for j in range(len(keylist)):
+        atoms=images[keylist[j]]
+        fp=fingerprintDB[keylist[j]]
+        for i in range(len(fp)):
+            atom=atoms[i]
+            nAtomsDict[atom.symbol][j]+=1
+    
+    atomsPositions={}
+    for element in elements:
+        atomsPositions[element]=np.cumsum(nAtomsDict[element])-nAtomsDict[element]
+
+    atomsIndsReverse={}
+    for element in elements:
+        atomsIndsReverse[element]=[]
+        for i in range(len(keylist)):
+            if nAtomsDict[element][i]>0:
+                atomsIndsReverse[element].append(np.ones((nAtomsDict[element][i],1))*i)
+        if len(atomsIndsReverse[element])>0:
+            atomsIndsReverse[element]=np.concatenate(atomsIndsReverse[element])
+        
+    atomArraysAll={}
+    for element in elements:
+        atomArraysAll[element]=[]
+
+    energies=np.zeros((len(keylist),1))
+    natoms=np.zeros((len(keylist),1))
+    for j in range(len(keylist)):
+        atoms=images[keylist[j]]
+        fp=fingerprintDB[keylist[j]]
+        for element in elements:
+            atomArraysTemp=[]
+            curatoms=[atom for atom in atoms if atom.symbol==element]
+            for i in range(len(curatoms)):
+                atomArraysTemp.append(fp[curatoms[i].index])
+            if len(atomArraysTemp)>0:
+                atomArraysAll[element].append(atomArraysTemp)
+        energies[j]=atoms.get_potential_energy()
+        natoms[j]=len(atoms)
+
+    for element in elements:
+        if len(atomArraysAll[element])>0:
+            atomArraysAll[element]=np.concatenate(atomArraysAll[element])
+        else:
+            atomArraysAll[element]=[]
+    return atomArraysAll,nAtomsDict,atomsIndsReverse,energies,natoms
--- regression/tfAmpNN_test.py
+++ regression/tfAmpNN_test.py
@@ -5,3 +5,10 @@ elementFPLengths={'Rh': 56, 'O': 56,'C':56,'H':56}
 
 tfAmpNN_instance=tfAmpNN(elementFingerprintLengths=elementFPLengths)
 tfAmpNN_instance.train(images,fingerprints,nEpochs=10000)
+
+print('starting get_energy test!!')
+keylist=images.keys()
+for key  in keylist:
+    energypredict=tfAmpNN_instance.get_energy(key,images,fingerprints)[0][0]
+    expenergy=images[key].get_potential_energy()
+    print('exp: %1.2f, pred: %1.2f, dE=%1.3f'%(expenergy,energypredict,energypredict-expenergy))
