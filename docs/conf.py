91f6dcb245b61cd8b78b383a8196217e6cddc590
Andrew Peterson <andrew_peterson@brown.edu>
2016-03-09 13:12:57 -0500

    New logo draft.

--- docs/conf.py
+++ docs/conf.py
@@ -127,7 +127,8 @@ html_theme = 'bizstyle'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/amp.png'
+#html_logo = '_static/amp.png'
+html_logo = '_static/amp-logo.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
