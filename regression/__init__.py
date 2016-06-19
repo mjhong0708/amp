96877b25e5a22747ebc0745eab102aa55e11403b
Alireza <Alireza_Khorshidi@brown.edu>
2016-06-19 13:10:48 -0400

    The regressor can now be defined as an optimizer taking the loss function alone (without gradient).

--- regression/__init__.py
+++ regression/__init__.py
@@ -9,9 +9,15 @@ class Regressor:
     decides how to adjust the parameters to reduce this cost function.
     Global optimization conditioners (e.g., simulated annealing, etc.) can
     be built into this class.
+
+    :param lossprime: Decides whether or not the regressor needs to be fed in
+                      by gradient of the loss function as well as the loss
+                      function itself.
+    :type lossprime: boolean
+
     """
 
-    def __init__(self, optimizer=None, optimizer_kwargs=None):
+    def __init__(self, optimizer=None, optimizer_kwargs=None, lossprime=True):
         """optimizer can be specified; it should behave like a
         scipy.optimize optimizer. That is, it should take as its first two
         arguments the function to be optimized and the initial guess of the
@@ -24,6 +30,7 @@ class Regressor:
             optimizer_kwargs = {}
         self.optimizer = optimizer
         self.optimizer_kwargs = optimizer_kwargs
+        self.lossprime = lossprime
 
     def regress(self, model, log):
         """Performs the regression. Calls model.get_loss,
@@ -39,8 +46,11 @@ class Regressor:
         log(' Optimizer kwargs: %s' % self.optimizer_kwargs)
         x0 = model.vector.copy()
         try:
-            self.optimizer(model.get_loss, x0, model.get_lossprime,
-                           **self.optimizer_kwargs)
+            if self.lossprime:
+                self.optimizer(model.get_loss, x0, model.get_lossprime,
+                               **self.optimizer_kwargs)
+            else:
+                self.optimizer(model.get_loss, x0, **self.optimizer_kwargs)
         except ConvergenceOccurred:
             log('...optimization successful.', toc='opt')
             return True
