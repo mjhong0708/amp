1d4a87cd2706e6bb94850bd6912e09f12c3b3885
Andrew Peterson <andrew_peterson@brown.edu>
2017-01-12 09:58:05 -0500

    ASE import location changed for fcc100.

--- tests/misc_test/fpplot_test.py
+++ tests/misc_test/fpplot_test.py
@@ -11,7 +11,7 @@ import os
 from ase import Atoms, Atom, units
 import ase.io
 from ase.calculators.emt import EMT
-from ase.lattice.surface import fcc110
+from ase.build import fcc110
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 from ase.md import VelocityVerlet
 from ase.constraints import FixAtoms
