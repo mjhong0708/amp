cc07c54247a3725f67ae07e5a98b94e7fedb5b4b
Muammar El Khatib <muammarelkhatib@brown.edu>
2017-06-02 14:12:01 -0400

    In this commit kernel is computed per atom.

--- amp/model/kernel_ridge.py
+++ amp/model/kernel_ridge.py
@@ -17,25 +17,100 @@ class KRR(Model):
     ----------
     kernel : str
         Choose the kernel. Default is 'linear'.
-    alpha : float
+    sigma : float
         Conditioning factor.
     """
-    def __init__(self, alpha=1, kernel='linear', gamma=None, degree=3, coef0=1,
+    def __init__(self, sigma=1., kernel='linear', lamda=None, degree=3, coef0=1,
             kernel_parwms=None, mode=None, version=None, fortran=True):
         p = self.parameters = Parameters()
         p.version = version
         p.importname = '.model.kernel_ridge.KRR'
         p.mode = mode
 
-        self.alpha = alpha
-        self.kernel=kernel
+        self.sigma = sigma
+        self.kernel = kernel
 
-        self.clf = KernelRidge(alpha=self.alpha)
+    def kernel_matrix(self, afp, kernel='rbf'):
+        """This method takes as arguments a feature vector and a string that refers
+        to the kernel type used.
+
+        Parameters
+        ----------
+        features : list or numpy array
+            Column vector containing the fingerprint of the atoms in images.
+        kernel : str
+            Select the kernel to be used. Supported kernels are: 'linear',
+            rbf'.
+
+        Returns
+        -------
+        K : array
+            The kernel matrix.
+
+        Notes
+        -----
+        Kernels may differ a lot between them. The kernel_matrix method has to
+        contain algorithms needed to build the desired matrix. The computation
+        of the kernel, is done by auxiliary functions that are located at the
+        end of the KRR class.
+        """
+
+        afp = np.asarray(afp)
+        print(afp)
+
+        if kernel == 'linear':
+            K = linear(afp)
+
+        elif kernel == 'rbf':
+            K = [ rbf(x, afp, sigma=self.sigma) for x in afp ]
+
+            """This is for testing purposes
+            xjs = np.array([
+                0.,
+                0.05263158,
+                0.10526316,
+                0.15789474,
+                0.21052632,
+                0.26315789,
+                0.31578947,
+                0.36842105,
+                0.42105263,
+                0.47368421,
+                0.52631579,
+                0.57894737,
+                0.63157895,
+                0.68421053,
+                0.73684211,
+                0.78947368,
+                0.84210526,
+                0.89473684,
+                0.94736842,
+                1.
+                ])
+            K = np.array([rbf(x, xjs, sigma=self.sigma) for x in xjs])
+            print(K)
+            """
+        return K
 
     def fit(self, trainingimages, descriptor, log, parallel):
-        """Method to fit"""
-        print(trainingimages)
-        print(descriptor)
+        """This method fits the kernel ridge model using a loss function.
+
+        Parameters
+        ----------
+        trainingimages : dict
+            Hashed dictionary of training images.
+        descriptor : object
+            Class representing local atomic environment.
+        log : Logger object
+            Write function at which to log data. Note this must be a callable
+            function.
+        parallel: dict
+            Parallel configuration dictionary. Takes the same form as in
+            amp.Amp.
+        """
+
+        #print(trainingimages)
+        #print(descriptor)
         self._parallel = parallel
         self._log = log
 
@@ -44,57 +119,57 @@ class KRR(Model):
         tp.descriptor = descriptor
         tp.fingerprints = tp.descriptor.fingerprints
 
-        print(tp.fingerprints)
-
-        images = []
-        fingerprints = []
-        hashes = []
-
+        feature_matrix = []
+        suma = 0
         for hash, image in tp.trainingimages.iteritems():
-            #print(image.get_potential_energy())
-            hashes.append(hash)
-            images.append(image.get_potential_energy())
-            _fingerprints = []
-            for element in tp.fingerprints[hash]:
-                _fingerprints.append(element[1])
-                #print(_fingerprints)
-                #print(len(_fingerprints))
-            fingerprints.append(_fingerprints)
-            print(len(fingerprints))
+            suma += 1
+            features = []
+            for element, afp in tp.fingerprints[hash]:
+                #afp = np.asarray(afp)[:, np.newaxis]    # I append in column vectors
+                print(element)
+                afp = np.asarray(afp)
+                features.append(afp)
+                #features.append(self.kernel_matrix(afp, kernel=self.kernel)) I don't think this is correct
 
-        # Converting type from list to np.array
-        fingerprints = np.array(fingerprints)
-        targets = images
-        print(targets)
-        print(fingerprints.shape)
-        n_targets = fingerprints.shape[0]
-        n_features = fingerprints.shape[1] * fingerprints.shape[2]
-        print(n_targets, n_features)
-        features = fingerprints.reshape(n_targets, n_features)
+            feature_matrix.append(features)
+            #kernel = self.kernel_matrix(features[0], kernel=self.kernel)
+            print(suma)
+            #print(kernel)
+        for _ in feature_matrix:
+            kernel = self.kernel_matrix(_, kernel=self.kernel)
+            print(kernel)
+            exit()
 
-        """
-        print(features.shape)
-        K = np.dot(features, features.T)
-        print(K)
-        I = np.identity(K.shape[0])
-        print(K)
-        print(I)
-        sample_weight = np.empty(n_targets)
-        copy = "precomputed"
-        self.dual_coef_ = _solve_cholesky_kernel(K, targets, self.alpha, sample_weight, copy)
-        print(self.dual_coef_)
+        #"""
+        #Create a kernel dict
+        #"""
+        #k = { }
 
-        print(np.dot(features[0], self.dual_coef_))
-        """
-        self.clf.fit(features, targets)
+        #for element, afp in tp.fingerprints.iteritems():
+        #    print(element)
+        #    print(afp)
+        #    print('Call kernel')
+        #    k[element] = self.kernel_matrix(afp, kernel=self.kernel)
+        #print(k)
 
-        print(self.clf.predict(features[0]))
-
-        exit()
-        pass
+        #for hash, image in tp.trainingimages.iteritems():
+        #    print(hash)
+        #    #print(dir(image))
 
     @property
     def forcetraining(self):
         """Force training"""
         pass
 
+"""
+Auxiliary functions to compute kernels
+"""
+def linear(x):
+    """ Compute a linear kernel """
+    linear = np.dot(afp, afp.T)
+    return linear
+
+def rbf(x, afp, sigma=1.):
+    """ Compute the rbf (AKA Gaussian) kernel.  """
+    rbf = np.exp(- (x - afp)**2 / (2 * sigma**2))
+    return rbf
