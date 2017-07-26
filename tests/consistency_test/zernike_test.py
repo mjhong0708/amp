0d93d9194cd26eeadcc4a61ef778e8346586ce21
Andrew Peterson <andrew_peterson@brown.edu>
2017-07-26 14:28:47 +0100

    Fixes to Zernike test.
    1. range(1,2) only gives [1] -> changed to range(1,3)
    2. dropped down to a single image rather than 3 for speed.

--- tests/consistency_test/zernike_test.py
+++ tests/consistency_test/zernike_test.py
@@ -9,6 +9,7 @@ import numpy as np
 from ase import Atoms
 from amp.descriptor.zernike import Zernike
 from amp.utilities import hash_images, assign_cores
+from amp.utilities import Logger
 
 # Making the list of images
 
@@ -27,41 +28,7 @@ def make_images():
                             [0.,  2.75064538,  8.37532269],
                             [3.89,  2.75064538,  8.37532269],
                             [5.835,  1.37532269,  8.5],
-                            [5.835,  7.12596807,  8.]])),
-              Atoms(symbols='Pd3O2',
-                    pbc=np.array([True,  True, False], dtype=bool),
-                    cell=np.array(
-                        [[7.78,   0.,   0.],
-                         [0.,   5.50129076,   0.],
-                            [0.,   0.,  15.37532269]]),
-                    positions=np.array(
-                        [[3.88430768e+00,   5.28005966e-03,
-                          8.36678641e+00],
-                            [-1.01122240e-02,   2.74577426e+00,
-                                8.37861758e+00],
-                            [3.88251383e+00,   2.74138906e+00,
-                                8.37087611e+00],
-                            [5.82067191e+00,   1.19156898e+00,
-                                8.97714483e+00],
-                            [5.83355445e+00,   7.53318593e+00,
-                             8.50142020e+00]])),
-              Atoms(symbols='Pd3O2',
-                    pbc=np.array([True,  True, False], dtype=bool),
-                    cell=np.array(
-                        [[7.78,   0.,   0.],
-                         [0.,   5.50129076,   0.],
-                            [0.,   0.,  15.37532269]]),
-                    positions=np.array(
-                        [[3.87691266e+00,   9.29708987e-03,
-                          8.35604207e+00],
-                            [-1.29700138e-02,   2.74373753e+00,
-                                8.37941484e+00],
-                            [3.86813484e+00,   2.73488653e+00,
-                                8.36395999e+00],
-                            [5.80386111e+00,   7.98192190e-01,
-                                9.74324179e+00],
-                            [5.83223956e+00,   8.23855393e+00,
-                             9.18295137e+00]]))]
+                            [5.835,  7.12596807,  8.]]))]
 
     return images
 
@@ -71,14 +38,20 @@ def make_images():
 
 def test():
 
+    log.tic()
     images = make_images()
+    log('images made', toc=True)
     images = hash_images(images, ordered=True)
+    log('images hashed', toc=True)
 
     ref_fps = {}
     ref_fp_primes = {}
     count = 0
     for fortran in [True, False]:
-        for ncores in range(1, 2):
+        log('count {}'.format(count), toc=True)
+        log('fortran {}'.format(str(fortran)), toc=True)
+        for ncores in range(1, 3):
+            log('ncores {}'.format(ncores), toc=True)
             cores = assign_cores(ncores)
             descriptor = Zernike(fortran=fortran,
                                  dblabel='Zernike-%s-%d' % (fortran, ncores))
@@ -87,7 +60,9 @@ def test():
                                                         'envcommand': None},
                                               log=None,
                                               calculate_derivatives=True)
+            log('calculated fingerprints', toc=True)
             for hash, image in images.items():
+                log('checking image {}'.format(hash), toc=True)
                 if count == 0:
                     ref_fps[hash] = descriptor.fingerprints[hash]
                     ref_fp_primes[hash] = descriptor.fingerprintprimes[hash]
@@ -113,4 +88,5 @@ def test():
             count += 1
 
 if __name__ == '__main__':
+    log = Logger(file='log.txt')
     test()
