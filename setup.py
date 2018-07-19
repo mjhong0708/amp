5de5d59844e92cb87dc5f6848ab473206e57f199
Andrew Peterson <andrew_peterson@brown.edu>
2018-07-19 11:06:10 -0400

    Expanded setup.py file.

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 
 from numpy.distutils.core import Extension, setup
 
+from amp.__version__ import version
+
 
 fmodules = Extension(name='amp.fmodules',
                      sources=['amp/model/neuralnetwork.f90',
@@ -10,14 +12,21 @@ fmodules = Extension(name='amp.fmodules',
                               'amp/descriptor/zernike.f90',
                               'amp/model.f90'])
 
-setup(name='amp', version='dev', long_description=open('README.md').read(),
+setup(name='amp',
+      version=version,
+      description='Atomistic Machine-learning Package',
+      long_description=open('README.md').read(),
       packages=['amp', 'amp.descriptor', 'amp.regression', 'amp.model'],
       package_dir={'amp': 'amp', 'descriptor': 'descriptor',
-                   'regression': 'regression', 'model': 'model'},
+                   'regerressiosn': 'regression', 'model': 'model'},
       classifiers=['Programming Language :: Python',
                    'Programming Language :: Python :: 2.6',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.3'],
       install_requires=['numpy>=1.7.0', 'matplotlib', 'ase'],
-      ext_modules=[fmodules])
+      ext_modules=[fmodules],
+      author='Andrew Peterson',
+      author_email='andrew_peterson@brown.edu'
+      url='https://bitbucket.org/andrewpeterson/amp',
+     )
