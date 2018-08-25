fa04fc8850c75943f8379e596c48c38cdbd60a20
Alireza Khorshidi <alireza_khorshidi@brown.edu>
2018-08-25 00:55:50 -0400

    Fortran cutoff function calculations made more efficient.

--- amp/descriptor/cutoffs.f90
+++ amp/descriptor/cutoffs.f90
@@ -8,26 +8,20 @@
           double precision, optional:: p_gamma
           integer:: cutofffn_code
 
-!       To avoid noise, for each call of this function, it is better to
-!       set returned variables to 0.0d0.
-        cutoff_fxn = 0.0d0
+          ! To avoid noise, for each call of this function, it is better to
+          ! set returned variables to 0.0d0.
+          cutoff_fxn = 0.0d0
 
-        if (cutofffn_code == 1) then
-            if (r > rc) then
-                cutoff_fxn = 0.0d0
-            else
-                pi = 4.0d0 * datan(1.0d0)
-                cutoff_fxn = 0.5d0 * (cos(pi*r/rc) + 1.0d0)
-            end if
-        elseif (cutofffn_code == 2) then
-            if (r > rc) then
-                cutoff_fxn = 0.0d0
-            else
-                cutoff_fxn = 1. + p_gamma &
+          if (r < rc) then
+              if (cutofffn_code == 1) then
+                  pi = 4.0d0 * datan(1.0d0)
+                  cutoff_fxn = 0.5d0 * (cos(pi*r/rc) + 1.0d0)
+              elseif (cutofffn_code == 2) then
+                  cutoff_fxn = 1. + p_gamma &
                       * (r / rc) ** (p_gamma + 1) &
                       - (p_gamma + 1) * (r / rc) ** p_gamma
-            end if
-        endif
+              end if
+          end if
       end function cutoff_fxn
 
       function cutoff_fxn_prime(r, rc, cutofffn_code, p_gamma)
@@ -36,21 +30,15 @@
           double precision, optional:: p_gamma
           integer:: cutofffn_code
 
-!       To avoid noise, for each call of this function, it is better to
-!       set returned variables to 0.0d0.
+          ! To avoid noise, for each call of this function, it is better to
+          ! set returned variables to 0.0d0.
           cutoff_fxn_prime = 0.0d0
 
-          if (cutofffn_code == 1) then
-              if (r > rc) then
-                  cutoff_fxn_prime = 0.0d0
-              else
+          if (r < rc) then
+              if (cutofffn_code == 1) then
                   pi = 4.0d0 * datan(1.0d0)
                   cutoff_fxn_prime = -0.5d0 * pi * sin(pi*r/rc) / rc
-              end if
-          elseif (cutofffn_code == 2) then
-              if (r > rc) then
-                  cutoff_fxn_prime = 0.0d0
-              else
+              elseif (cutofffn_code == 2) then
                   cutoff_fxn_prime = (p_gamma * (p_gamma + 1) / rc) &
                    * ((r / rc) ** p_gamma - (r / rc) ** (p_gamma - 1))
               end if
