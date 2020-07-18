ba8188cb91a403f37a5e3df0d565430aacd43348
Cheng Zeng <cheng_zeng1@brown.edu>
2020-07-18 17:33:33 -0400

    add offsets and use different numbers of symmetry functions in the example

--- tests/test_gaussian_neural_g5.py
+++ tests/test_gaussian_neural_g5.py
@@ -47,14 +47,14 @@ def train_test():
     train_images = generate_data(2)
     elements = ['Pt', 'Cu']
     G = make_symmetry_functions(elements=elements, type='G2',
-                etas=np.logspace(np.log10(0.05), np.log10(5.),
-                                 num=4))
+                etas=np.logspace(np.log10(0.05), np.log10(5.), num=4),
+                offsets=[0, 2.])
     G += make_symmetry_functions(elements=elements, type='G5',
                  etas=[0.005],
                  zetas=[1., 4.],
                  gammas=[+1., -1.])
 
-    G = {element: G for element in elements}
+    G = {element: G[::_] for element, _ in zip(elements, [1, 2])}
 
     calc = Amp(descriptor=Gaussian(Gs=G),
                model=NeuralNetwork(hiddenlayers=(3, 3)),
